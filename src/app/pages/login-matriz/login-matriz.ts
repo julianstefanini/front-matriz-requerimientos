3c8a41d3aaef6b19a02328f181b437d89420b17e
julianstefanini <jrsantos5@stefanini.com>
2025-12-03 10:18:21 -0500

    Finalizacion front login

--- src/app/pages/login-matriz/login-matriz.ts
+++ src/app/pages/login-matriz/login-matriz.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-matriz',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './login-matriz.html',
   styleUrl: './login-matriz.scss',
 })
 export class LoginMatriz {
-
+  usuarioFormControl = new FormControl('', Validators.required);
+  contraseniaFormControl = new FormControl('', Validators.required);
 }
